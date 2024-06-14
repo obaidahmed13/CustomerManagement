5e60d7eae0f4dff4d790433f062e73ebc6c96f51
obaidahmed13 <obaidahmed738@gmail.com>
2024-06-14 14:01:19 -0400

    commit

---
+++ src/main/java/genspark/customermanagement/CustomerManagementNew/CustomerManagementNewApplication.java
@@ -0,0 +1,13 @@
+package genspark.customermanagement.CustomerManagementNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/genspark/customermanagement/CustomerManagementNew/CustomerManagementNewApplicationTests.java
@@ -0,0 +1,13 @@
+package genspark.customermanagement.CustomerManagementNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
